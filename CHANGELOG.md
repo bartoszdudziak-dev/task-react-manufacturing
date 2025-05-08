e7657e47b998ec4573f1801338e23f2a5ee63abe
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2025-05-08 17:28:02 +0200

    fix: firabase new config

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Pages routes
 - Firebase configuration
 - Modal Compound Component
+- TanStack Query configuration
 
 ### Changed
 
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,24 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyC9BSVwmacXFc65czR8KGtcY4RQX8rpzsY',
-  authDomain: 'job-board-cada9.firebaseapp.com',
-  projectId: 'job-board-cada9',
-  storageBucket: 'job-board-cada9.firebasestorage.app',
-  messagingSenderId: '1001821791590',
-  appId: '1:1001821791590:web:40e57a9791920e1db494a7',
-  measurementId: 'G-0MCTF3NN5R',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
